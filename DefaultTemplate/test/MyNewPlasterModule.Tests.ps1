cd6d0e96c9ca1ed9883bb636a87de409e4147049
Mark Warneke <mawarnek@microsoft.com>
2017-12-08 13:31:45 +0100

    Adds Default Plaster Manifest

---
+++ DefaultTemplate/test/MyNewPlasterModule.Tests.ps1
@@ -0,0 +1,9 @@
+$ModuleManifestName = 'MyNewPlasterModule.psd1'
+$ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
+
+Describe 'Module Manifest Tests' {
+    It 'Passes Test-ModuleManifest' {
+        Test-ModuleManifest -Path $ModuleManifestPath
+        $? | Should Be $true
+    }
+}
