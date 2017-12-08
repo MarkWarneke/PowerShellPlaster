bcc79efdb1d30c6e45ec98e87f888ccbc2102216
Mark Warneke <mawarnek@microsoft.com>
2017-12-08 14:02:43 +0100

    renaming folder structur

--- New-PlasterManifestParameterSplatting.ps1
+++ New-PlasterManifestParameterSplatting.ps1
@@ -1,7 +1,19 @@
-$destinationFolder = '.\MarkMitKTemplate'
+$destination = @{
+    Manifest ='.\MarkMitKPlasterManifestTemplate'
+    Module = '.\MarkMitKModule'
+}
+
+Find-Module -Name Plaster -Repository PSGallery |
+    Install-Module -Verbose -Force
+
+foreach ($path in $destination.GetEnumerator()) {
+    if (-Not (Test-Path -Path $path.Value)) {
+        New-Item -Path $path.Value -ItemType Directory
+    }
+}
 
 $manifestProperties = @{
-    Path = "$destinationFolder\MarkMitKPlasterManifest.xml"
+    Path = "$($destination.ManifestFolder)\MarkMitKPlasterManifest.xml"
     Title = "MarkMitK working with Plaster"
     TemplateName = "MarkMitKPlasterTemplate"
     TemplateVersion = "0.0.1"
@@ -12,3 +24,5 @@ $manifestProperties = @{
 
 New-PlasterManifest @manifestProperties
 code $destinationFolder
+
+Invoke-Plaster -TemplatePath $($destination.Manifest) -DestinationPath $($destination.Module)
