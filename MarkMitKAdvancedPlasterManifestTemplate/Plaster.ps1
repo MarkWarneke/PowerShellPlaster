1e9b7393b0f07df8659c26d62041e3624176eae5
Mark Warneke <mawarnek@microsoft.com>
2017-12-13 16:08:15 +0100

    parameterized plaster generator

--- MarkMitKAdvancedPlasterManifestTemplate/Plaster.ps1
+++ MarkMitKAdvancedPlasterManifestTemplate/Plaster.ps1
@@ -1,16 +1,23 @@
-
+[CmdletBinding()]
+param (
+    [string] $NewModuleName = "MarkMitKModule"
+)
 
 $plaster = @{
     TemplatePath    = "." 
-    DestinationPath = "C:\tmp\NewModule" # Must be named exactly like ModuleName for tests
+    DestinationPath = "C:\dev\git\AdvancedPlaster\$NewModuleName"  # Must be named exactly like ModuleName for tests
     FullName = "Mark Warneke"
-    ModuleName = "NewModule"
+    ModuleName = "$NewModuleName"
     ModuleDesc = "Here is a module description"
     Version = "0.1.0"
     FunctionName = "Get-Demo"
 }
 
+
+
 If (!(Test-Path $plaster.DestinationPath)) {
     New-Item -ItemType Directory -Path $plaster.DestinationPath
+} else {
+    Remove-Item -Path $plaster.DestinationPath
 }
 Invoke-Plaster @plaster -Verbose
